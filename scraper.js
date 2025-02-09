491a66def840c28eda57e5b76001f4056eebb37c
matan654 <matan.7.8.1991@gmail.com>
2025-02-09 11:33:16 +0200

    Add User-Agent and headers

--- scraper.js
+++ scraper.js
@@ -6,6 +6,11 @@ const config = require('./config.json');
 const getYad2Response = async (url) => {
     const requestOptions = {
         method: 'GET',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5'
+        },
         redirect: 'follow'
     };
     try {
